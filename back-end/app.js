b8770d3b56776b3d28aa5cfc1baa874d3bc03b3f
Carlos Navarro <kandres38@gmail.com>
2023-03-31 22:48:38 -0500

    prueb3

--- back-end/app.js
+++ back-end/app.js
@@ -3,20 +3,6 @@ const express = require('express')
 const app = express()
 
 
-const User=require('./models').user
-
-////aqui hacemos las importaciones y que todo quede dentro de ella
-
-const dotenv = require('dotenv');
-const cors = require('cors')
-// const session = require('express-session');
-//const { auth } = require('express-openid-connect');
-const morgan = require('morgan')
-dotenv.config()
-
-require('./index')
-
-
 const port= 4000
 
 app.listen(port, () => {
--- back-end/routes/routeGoogle/route.js
+++ back-end/routes/routeGoogle/route.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const passport= require('passport');
 const session = require('express-session');
 const googleController = require('../../controllers/google/googlePassportController')
-const app=express
+const app=require('../../app')
 
 app.use(passport.initialize());
 app.use(session({
@@ -19,9 +19,10 @@ app.use(session({
 router.get('/google', googleController);
 
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),(req, res)=> {
+  res.redirect('http://localhost:5173/usuarioLog')
 
 
-  res.status(200).json({message: 'Success',user:{id:req.user.id,name:req.user.name,email:req.user.email}})
+  //res.status(200).json({message: 'Success',user:{id:req.user.id,name:req.user.name,email:req.user.email}})
    
  })
 
